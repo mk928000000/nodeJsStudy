e3a3222147d3e5a7d46b5eae337a8048888f4268
MINGYEONG JEONG <72364842+mk928000000@users.noreply.github.com>
2023-05-15 10:59:44 +0900

    코딩애플 따라하기
    restful api 란?

--- node_mongo/server.js
+++ node_mongo/server.js
@@ -1,12 +1,16 @@
 /*
  *node.js 에서 서버를 만들기 위한 기본 문법 세줄
+ * // 아까 설치한 라이브러리를 첨부
+ * // 첨부한 라이브러리로 객체 만듬
+ * // 58080포트에 서버를 어디에다 열겠다.
  *
  */
 
-// 아까 설치한 라이브러리를 첨부
 const express = require("express");
-// 첨부한 라이브러리로 객체 만듬
 const app = express();
+//body parser 라이브러리가 express 에 포함되어있다. 사용하도록 한줄 추가.
+app.use(express.urlencoded({ extended: true }));
+
 // 58080포트에 서버를 어디에다 열겠다.
 app.listen(58080, function () {
   // 터미널에 node server.js 를 치면 아래 콘솔문이 뜬다. 확인 후 웹창에 localhost:58080 을 검색하면 서버 화면이 열린다.
@@ -32,4 +36,21 @@ app.get("/", function (req, res) {
 });
 app.get("/write", function (req, res) {
   res.sendFile(__dirname + "/write.html");
+});
+
+/*
+ * 함수 안에 함수 (function(){}) = 콜백함수
+ * 순차적으로 실행하고 싶을때 쓴다
+ * 신문법으로 화살표 함수도 사용 가능.
+ * () => {}
+ *
+ */
+
+// /add 경로로 POST 요청을 하면 아래 코드를 실행
+// 보낸 정보는 req 안에 있다. (꺼내 쓰려면 라이브러리 필요 body-parser)
+app.post("/add", function (req, res) {
+  res.send("전송완료");
+  console.log(req.body.title);
+  console.log(req.body.date);
+  //DB 에 저장하라 (Rest API)
 });
